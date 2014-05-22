1e6071287ce7c2ca09486d78115673935de3f203
nayamo <naya@val.co.jp>
2014-05-22 16:10:51 +0900

    json/ssh/host.json にserverspecをかけるhostとssh_keyファイルを設定できるようにした

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'serverspec'
 require 'pathname'
+require 'json'
+require 'pp'
 require 'net/ssh'
 
 include SpecInfra::Helper::Ssh
@@ -19,11 +21,13 @@ RSpec.configure do |c|
     else
       file = block.source_location.first
     end
-    host  = File.basename(Pathname.new(file).dirname)
+    host_json = JSON.parse( File.read( File.dirname(__FILE__)+'/../json/ssh/host.json' ) )
+    host = host_json['host']
     if c.host != host
       c.ssh.close if c.ssh
       c.host  = host
       options = Net::SSH::Config.for(c.host)
+      options[:key_data] = File.read( host_json['ssh_key'] ) if host_json['ssh_key']
       user    = options[:user] || Etc.getlogin
       c.ssh   = Net::SSH.start(host, user, options)
     end
